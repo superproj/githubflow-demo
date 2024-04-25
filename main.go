d35249bd601b124502e66e1f381e69de834920ae
孔令飞 <konglingfei.nosbelm@bytedance.com>
2024-04-25 16:37:43 +0800

    feat: add sub function
    Change-Id: I6eb9ec8641233dbe2ce578cc4d0e1e4131c6cea7

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ func add(a, b int) int {
 	return a + b
 }
 
+func sub(a, b int) int {
+	return a - b
+}
+
 func main() {
 	num1 := 10
 	num2 := 20
 
 	sum := add(num1, num2)
+	diff := sub(num1, num2)
+
 	fmt.Printf("The sum of %d and %d is: %d\n", num1, num2, sum)
+	fmt.Printf("The difference between %d and %d is: %d\n", num1, num2, diff)
 }
